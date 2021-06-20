86994d339372079d4437e9755d581b6709e1ef25
laihm98 <laihanming@gmail.com>
2021-06-21 00:37:37 +0800

    Update accountSettingScreen.js

--- screens/accountSettingScreen.js
+++ screens/accountSettingScreen.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import {View, StyleSheet, Text, Button, SafeAreaView, StatusBar, TouchableOpacity} from 'react-native';
+import {View, StyleSheet, Text, Button, SafeAreaView, StatusBar, TouchableOpacity, Alert, flexDirection} from 'react-native';
 import Header from "../components/header";
 import colors from "../common/colors";
 import commonStyle from "../common/commonStyles";
+import MaterialIcon from "react-native-vector-icons/MaterialIcons";
+import {vw} from "react-native-expo-viewport-units";
 
 function AccountSettingScreen({navigation}) {
+
+    const EditAvatar = () => {
+        Alert.alert(
+            "Edit Avatar", 
+            " ",
+            [{text:"Upload"},
+            {text:"Cancel"}]
+
+        );
+    }
+
+    const ProfileDetails = () => {
+        return (
+            <View style={styles.profileDetailsContainer}>
+                <View style={styles.avatarContainer}>
+                    <MaterialIcon name={'account-circle'} size={vw(35)}/>
+                </View>
+
+                <View style={styles.editButtonContainer}> 
+                    <TouchableOpacity style={styles.buttonSmall} onPress={EditAvatar}>
+                        <Text style={styles.editAvatarText}>Edit Avatar</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.detailContainer}>
+                    <Text style={styles.detailText}>xxxxxxxxx.gmail.com</Text>
+                </View>
+
+                <View style={styles.detailContainer}>
+                    <Text style={styles.detailText}>Account Type: Premium</Text>
+                </View>
+
+            </View>
+        )
+    }
 
     const button1Action = () => {
         // navigation.navigate('Home')
@@ -18,9 +55,8 @@ function AccountSettingScreen({navigation}) {
             <StatusBar/>
             <Header navigation={navigation} text={'Account Settings'} backEnabled={true}/>
             <View style={styles.contentContainer}>
-                <View>
-                    <Text>Account Setting Screen</Text>
-                </View>
+                <ProfileDetails/>
+                <View style={styles.divider}/>
             </View>
             <View style={styles.bottomContainer}>
                 <TouchableOpacity style={[commonStyle.buttonDual, commonStyle.dropShadow]} onPress={button1Action}>
@@ -43,13 +79,15 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         height: '100%',
     },
+
     contentContainer: {
         flex: 3,
-        backgroundColor: '#D0D0D0',
+        backgroundColor: colors.background,
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center'
     },
+    
     bottomContainer: {
         flex: 1,
         // backgroundColor: '#183fc8',
@@ -57,6 +95,77 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'flex-end'
     },
+
+    profileDetailsContainer:{
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 5,
+        marginBottom: 10,
+
+    },
+    
+    avatarContainer: {
+        backgroundColor: colors.primaryColor,
+        width: vw(35),
+        height: vw(35),
+        borderRadius: vw(35 / 2),
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom:10,
+    }, 
+    
+    editButtonContainer:{
+        width: vw(65),
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    
+    buttonSmall: {
+        backgroundColor: colors.primaryColor,
+        width: vw(20),
+        height: vw(10),
+        borderRadius: vw(10),
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    nameContainer:{
+
+    },
+
+    detailContainer:{
+        width: vw(65),
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
+       
+    },
+
+    detailText:{
+        fontSize: 15,
+    },
+
+    editAvatarText:{
+        fontSize: 13,
+    },
+
+    accountTypeContainer:{
+
+    },
+
+    divider: {
+        width: vw(85),
+        marginVertical: 10,
+        borderBottomColor: '#656565',
+        borderBottomWidth: 1,
+    },
+
 })
 
 export default AccountSettingScreen;

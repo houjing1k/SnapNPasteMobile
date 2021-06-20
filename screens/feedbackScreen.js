8e0d27008c5003a35929e363d7dbba5e5cf82be0
laihm98 <laihanming@gmail.com>
2021-06-20 22:43:49 +0800

    Update feedbackScreen.js

--- screens/feedbackScreen.js
+++ screens/feedbackScreen.js
@@ -9,21 +9,82 @@ import {View,
         Alert, 
         TextInput,
         KeyboardAvoidingView,
-        flexWrap
+        flexWrap,
         } from 'react-native';
 import Header from "../components/header";
 import colors from "../common/colors";
 import commonStyle from '../common/commonStyles';
+import { useEffect, useState } from 'react' ;
 
 function FeedbackScreen({navigation}) {
 
-    const button1Action = () => {
-        Alert.alert(title= "Feedback Submission", message= "Successful")
-        // navigation.navigate('Home')
+    const [data, setData] = useState({
+        Title: '',
+        Feedback: '',
+        
+    })
+
+    const Submit = async () => {
+        if (data.isValidTitle && data.isValidFeedback) {
+            await axios.post('http://byteus.me:8000/feedback', {
+                title: data.Title,
+                feedback: data.Feedback,
+            })
+                .then((response) => {
+                    console.log(response);
+                    Alert.alert(
+                        "Submission Successful",
+                        "Your feedback has been sent to us. Thank you!",
+                        [
+                            {
+                                text: "OK", onPress: () => {
+                                    navigation.pop();
+                                }
+                            }]
+                    );
+                })
+                .catch((error) => {
+                    console.log(error);
+                    Alert.alert(
+                        "Error",
+                        "Submission unsuccessful, try again.",
+                        [{
+                            text: "OK", onPress: () => {
+                            }
+                        }]
+                    );
+                });
+        } else {
+            Alert.alert(
+                "Invalid Input",
+                (!data.isValidTitle ? 'Please input title!\n' : '') +
+                (!data.isValidFeedback ? 'Please input feedback!\n' : '') +
+                [{text: "OK"}]
+            )
+        }
+
+
     }
-    const button2Action = () => {
-        // navigation.navigate('Signup');
+
+    const handleTitleChange = (val) => {
+        setData({
+            ...data,
+            Title: val,
+            isValidTitle: val !== '',
+        });
     }
+    
+    const handleFeedbackChange = (val) => {
+        setData({
+            ...data,
+            Feedback: val,
+            isValidFeedback: val !== '',
+        });
+    }
+
+    useEffect(() => {
+        console.log(data);
+    }, [data])
 
     return (
         <KeyboardAvoidingView style={styles.container}>
@@ -42,6 +103,7 @@ function FeedbackScreen({navigation}) {
                             <TextInput
                                 style={[styles.input, {width: 220}]}
                                 placeholder={"What's the issue?"}
+                                onChangeText={(val) => handleTitleChange(val)}
                             />
                         </View>
                     </View>
@@ -54,9 +116,10 @@ function FeedbackScreen({navigation}) {
                         </View>
                         <View style={styles.inputFieldContainerBig} flexDirection= 'row'>
                             <TextInput
+                                multiline
                                 style={[styles.input, {width: 220}]}
                                 placeholder={"How can we improve/fix it?"}
-                               
+                                onChangeText={(val) => handleFeedbackChange(val)}
                                 
                                     
                             />
@@ -69,7 +132,7 @@ function FeedbackScreen({navigation}) {
                 </View>  */}
 
                 <View style={styles.bottomContainer}>
-                    <TouchableOpacity style={[commonStyle.buttonDual, commonStyle.dropShadow]} onPress={button1Action}>
+                    <TouchableOpacity style={[commonStyle.buttonDual, commonStyle.dropShadow]} onPress={Submit}>
                         <Text style={commonStyle.commonTextStyleLight}>Submit Feedback</Text>
                     </TouchableOpacity>
                     {/* <TouchableOpacity style={styles.button} onPress={button2Action}>

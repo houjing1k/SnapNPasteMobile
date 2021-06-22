c60bbd0a61247e133616a73131f85aa380e3897d
laihm98 <laihanming@gmail.com>
2021-06-23 01:12:50 +0800

    Update historyScreen.js

--- screens/historyScreen.js
+++ screens/historyScreen.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {View, StyleSheet, Text, Button, SafeAreaView, StatusBar, TouchableOpacity} from 'react-native';
+import {View, StyleSheet, Text, Button, SafeAreaView, StatusBar, TouchableOpacity, Image} from 'react-native';
 import Header from "../components/header";
 import colors from "../common/colors";
 import commonStyle from "../common/commonStyles";
+import { vw, vh } from 'react-native-expo-viewport-units';
+import { color } from 'react-native-elements/dist/helpers';
 
 function HistoryScreen({navigation}) {
 
-    const button1Action = () => {
-        // navigation.navigate('Home')
+    const selectButton = () => {
+        //navigation.navigate('TextConfirmation')
     }
     const button2Action = () => {
         // navigation.navigate('Signup');
@@ -18,12 +20,21 @@ function HistoryScreen({navigation}) {
             <StatusBar/>
             <Header navigation={navigation} text={'History'} backEnabled={true}/>
             <View style={styles.contentContainer}>
-                <View>
-                    <Text>History Screen</Text>
+                <View style={styles.historyContainer}>
+                    <View styles={styles.historyItemContainer}>
+                        <Image source={{width:vw(35), height:vh(25), uri:"https://picsum.photos/200/300",}}/> 
+                        <Image source={{width:vw(35), height:vh(25), uri:"https://picsum.photos/200/300",}}/> 
+                    </View>
+                    <View styles={styles.historyItemContainer}>
+                        <Image source={{width:vw(35), height:vh(25), uri:"https://picsum.photos/200/300",}}/> 
+                        <Image source={{width:vw(35), height:vh(25), uri:"https://picsum.photos/200/300",}}/> 
+                    </View>
+
                 </View>
             </View>
+            
             <View style={styles.bottomContainer}>
-                <TouchableOpacity style={[commonStyle.buttonDual, commonStyle.dropShadow]} onPress={button1Action}>
+                <TouchableOpacity style={[commonStyle.buttonDual, commonStyle.dropShadow]} onPress={selectButton}>
                     <Text style={styles.buttonText}>Select</Text>
                 </TouchableOpacity>
                 {/* <TouchableOpacity style={styles.button} onPress={button2Action}>
@@ -45,7 +56,7 @@ const styles = StyleSheet.create({
     },
     contentContainer: {
         flex: 3,
-        backgroundColor: '#D0D0D0',
+        backgroundColor: colors.backgroundColor,
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center'
@@ -55,7 +66,30 @@ const styles = StyleSheet.create({
         // backgroundColor: '#183fc8',
         flexDirection: 'column',
         alignItems: 'center',
-        justifyContent: 'flex-end'
+        justifyContent: 'flex-start'
+    },
+
+    historyContainer:{
+        width: vw(85),
+        height: vh(65),
+        paddingLeft: 15,
+        paddingRight: 10,
+        marginVertical: 20,
+        borderWidth: 2,
+        borderColor: colors.color3,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 8,
+        backgroundColor: colors.primaryColor,
+    },
+
+    historyItemContainer:{
+        width: vw(85),
+        height: vw(65/2),
+        justifyContent: 'space-evenly',
+        backgroundColor: 'yellow'
+        
     },
 })
 
